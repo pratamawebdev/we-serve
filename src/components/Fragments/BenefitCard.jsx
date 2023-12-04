048b06d84b2eab0a3994a41f810c1ed3e324c419
Willy Pratama Cendana <111175973+pratamawebdev@users.noreply.github.com>
2023-12-04 21:24:10 +0700

    Update BenefitCard.jsx

--- src/components/Fragments/BenefitCard.jsx
+++ src/components/Fragments/BenefitCard.jsx
@@ -6,16 +6,6 @@ const BenefitCard = (props) => {
       <div className="flex flex-col">
         <h3 className="text-xl font-bold text-indigo-950">{title}</h3>
         <p className="text-base leading-relaxed text-gray-500">{description}</p>
-        <button>Tombol Tambah</button>
-        <div>
-          <a href="google.com">Google</a>
-        </div>
-        <p>Ini paragraf</p>
-        <span>hello</span>
-        <ul>
-          <li>makanan</li>
-          <li>minuman</li>
-        </ul>
       </div>
     </div>
   );
