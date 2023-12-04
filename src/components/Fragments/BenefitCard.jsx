b6098ee5d1b651c5142550bec0288a90dce8f8b5
Zikri Mansyursyah <zikrimansyursyah@gmail.com>
2023-12-04 20:59:41 +0700

    new: tambah tombol google

--- src/components/Fragments/BenefitCard.jsx
+++ src/components/Fragments/BenefitCard.jsx
@@ -7,6 +7,9 @@ const BenefitCard = (props) => {
         <h3 className="text-xl font-bold text-indigo-950">{title}</h3>
         <p className="text-base leading-relaxed text-gray-500">{description}</p>
         <button>Tombol Tambah</button>
+        <div>
+          <a href="google.com">Google</a>
+        </div>
         <p>Ini paragraf</p>
       </div>
     </div>
